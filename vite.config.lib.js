c5006d3cdef6cebe828c9209d705b82329bce725
rokobuljan <roko.cb@gmail.com>
2025-07-20 12:40:12 +0200

    imp: vite config

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -2,21 +2,12 @@
 
 import { defineConfig } from "vite";
 import postcssNesting from "postcss-nesting";
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const LIB_NAME = "jsort";
 
 export default defineConfig({
     root: "./src/lib",
     base: "./",
-    resolve: {
-        alias: {
-            '@lib': path.resolve(__dirname, `./src/lib/${LIB_NAME}.js`)
-        }
-    },
     build: {
         minify: "terser",
         lib: {
@@ -25,14 +16,6 @@ export default defineConfig({
             fileName: LIB_NAME,
             minify: true,
         },
-
-        // rollupOptions: {
-        //     output: {
-        //         chunkFileNames: "layouts/[name].js",
-        //         assetFileNames: `${LIB_NAME}.[ext]`, // Prevent renaming kioboard.css to style.css
-        //     }
-        // },
-        // sourcemap: false,
         outDir: '../../dist',
         emptyOutDir: true,
     },
