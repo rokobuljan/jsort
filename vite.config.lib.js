28270a23de174475d914f20dbc9520565211a722
rokobuljan <roko.cb@gmail.com>
2025-07-20 13:09:58 +0200

    fix: build terser minify

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -2,22 +2,65 @@
 
 import { defineConfig } from "vite";
 import postcssNesting from "postcss-nesting";
+import terser from '@rollup/plugin-terser';
 
 const LIB_NAME = "jsort";
 
 export default defineConfig({
     root: "./src/lib",
     base: "./",
+    
     build: {
-        minify: "terser",
+        outDir: '../../dist',
         lib: {
             entry: `${LIB_NAME}.js`,
-            name: LIB_NAME,
-            fileName: LIB_NAME,
-            minify: true,
         },
-        outDir: '../../dist',
-        emptyOutDir: true,
+        minify: "terser",
+        rollupOptions: {
+            output: [
+                // Unminified version
+                {
+                    format: 'es',
+                    entryFileNames: `${LIB_NAME}.js`,
+                },
+                // Minified version  
+                {
+                    format: 'es',
+                    entryFileNames: `${LIB_NAME}.min.js`,
+                    plugins: [terser()],
+                }
+            ]
+        }
+
+
+        // lib: {
+        //     entry: `${LIB_NAME}.js`,
+        //     name: LIB_NAME,
+        //     formats: ['es'],
+        //     fileName: () => `${LIB_NAME}.js`
+        // },
+        // rollupOptions: {
+        //     output: [
+        //         {
+        //             format: 'es',
+        //             entryFileNames: `${LIB_NAME}.js`,
+        //             minify: false
+        //         },
+        //         {
+        //             format: 'es',
+        //             entryFileNames: `${LIB_NAME}.min.js`,
+        //             minify: 'terser'
+        //         }
+        //     ]
+        // }
+        // minify: "terser",
+        // lib: {
+        //     entry: `${LIB_NAME}.js`,
+        //     name: LIB_NAME,
+        //     fileName: LIB_NAME,
+        //     minify: true,
+        //     formats: ['es'],
+        // },
     },
     css: {
         postcss: {
